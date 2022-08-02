c99dcb2f8334e018b2f9f341f70a0f78f624d235
Emiliano <emiliano@sturniolo.com.ar>
2022-08-02 11:48:52 -0300

    Fix unecessary comment

--- bisiesto.py
+++ bisiesto.py
@@ -2,7 +2,7 @@ inicio = 2024
 fin = (inicio+(600*4))
 for año in range(inicio, fin, 4):
     if año // 4 == año / 4 and año // 100 == año / 100:
-        # año = str(año)
+        año = str(año)
         print(año + " No es año bisiesto!")
     else:
         print(año)
